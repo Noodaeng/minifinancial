bb05eac30516e2d8ff9b9bb7a219b63ff5e009b6
Noodaeng <35387905+Noodaeng@users.noreply.github.com>
2026-07-22 20:19:55 +0700

    test-05

--- src/pages/settings/PortPage.vue
+++ src/pages/settings/PortPage.vue
@@ -39,6 +39,22 @@
               @onClickSave="onSave"
             />
           </q-card>
+          <!-- Testtttttttttttt -->
+          <div class="row q-col-gutter-md">
+            <div class="col-12 col-md-12">
+              <q-card class="bg-body text-appText flat bordered q-mb-md">
+                <PortComp
+                  ref="myChild"
+                  :custOption="custOption"
+                  :brokerOption="brokerOption"
+                  :portType="portType"
+                  :info="port"
+                  :enbBtnSave="canSave"
+                  @onClickSave="onSave"
+                />
+              </q-card>
+            </div>
+          </div>
         </div>
       </div>
     </div>
